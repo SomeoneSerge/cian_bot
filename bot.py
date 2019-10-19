1678ca82476933087bc1f2c7e797c99c5db44425
Serge V Kozlukov <newkozlukov@gmail.com>
2019-10-19 21:13:58 +0300

    basic package management

--- bot.py
+++ bot.py
@@ -1,31 +1,41 @@
 from telegram.ext import Updater
-from parser import get_flats
+from cian_parser import get_flats
 import datetime
 import logging
+import argparse
 
 logger = logging.getLogger('cian_bot')
 logger.setLevel(logging.DEBUG)
 
-url = 'https://www.cian.ru/cat.php?currency=2&deal_type=rent&engine_version=2' \
-      '&foot_min=15&maxprice=46000&metro%5B0%5D=77&metro%5B1%5D=85&metro%5B2%5D=129' \
-      '&offer_type=flat&only_foot=2&room1=1&room2=1&type=4'
-save_file = 'save.json'
-token = 'Token here'
-
-
-def update(bot, job):
-    flats = get_flats(url, save_file)
-    user_ids = ['User ids here']
-    if flats:
-        text = str(flats)
-        for id in user_ids:
-            bot.send_message(chat_id=id, text=text)
-    else:
-        for id in user_ids:
-            bot.send_message(chat_id=id, text='Nothing new, sorry.')
+URL = 'https://www.cian.ru/cat.php?currency=2&deal_type=rent&engine_version=2' \
+      '&foot_min=15&maxprice={maxprice}&metro%5B0%5D=77&metro%5B1%5D=85&metro%5B2%5D=129' \
+      '&offer_type={offer_type}&only_foot=2&room1=1&room2=1&type=4'
+SAVE_FILE = 'save.json'
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser('cian_bot')
+    parser.add_argument('--maxprice', default=1_000_000, type=int)
+    parser.add_argument('--offer-type', default='flat')
+    parser.add_argument('--token-file', default='.token')
+    parser.add_argument('--user', default=[], action='append')
+
+    args = parser.parse_args()
+    url = URL.format(**args.__dict__)
+    with open(args.token_file, 'r') as f:
+        token = f.readline().strip()
+
+    def update(bot, job):
+        flats = get_flats(url, SAVE_FILE)
+        user_ids = list(args.user)
+        if flats:
+            text = str(flats)
+            for id in user_ids:
+                bot.send_message(chat_id=id, text=text)
+        else:
+            for id in user_ids:
+                bot.send_message(chat_id=id, text='Nothing new, sorry.')
+                
     updater = Updater(token)
     job = updater.job_queue
     job.run_repeating(update, datetime.timedelta(minutes=30), 10)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(name='cian_bot',
+      author='galinova@sports.ru',
+      packages=['cian_parser'],
+      package_dir={'': 'src'},
+      install_requires=['requests'])
