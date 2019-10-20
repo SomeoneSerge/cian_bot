52e96167747b93431bc56696708803cd7d6c5f61
Serge V Kozlukov <newkozlukov@gmail.com>
2019-10-20 18:08:12 +0300

    fuck shit just aint returning whats quired, Im giving up for now

--- bot.py
+++ bot.py
@@ -6,7 +6,10 @@ import json
 import logging
 import os
 import os.path as osp
+import logging
+import copy
 
+import attr
 import requests
 
 import cian_parser
@@ -15,12 +18,56 @@ from telegram.ext import CommandHandler, Updater
 
 logger = logging.getLogger('cian_bot')
 logger.setLevel(logging.DEBUG)
+log_to_file = logging.FileHandler('cian_bot.log')
+log_to_file.setLevel(logging.DEBUG)
+logger.addHandler(log_to_file)
+log_to_stdout = logging.StreamHandler()
+log_to_stdout.setLevel(logging.INFO)
+logger.addHandler(log_to_stdout)
 
 SAVE_FILE = 'save.json'
 
+METRO = [
+    'Достоевская', 'Проспект Мира', 'Сухаревская', 'Цветной бульвар',
+    'Трубная', 'Чеховская', 'Пушкинская', 'Кузнецкий мост', 'Лубянка',
+    'Чистые пруды', 'Красные Ворота', 'Тургеневская', 'Сретенский бульвар',
+    'Китай-город', 'Чкаловская', 'Маяковская', 'Белорусская', 'Менделеевская',
+    'Новослободская'
+]
+METRO = [m.lower() for m in METRO]
+METRO_BLACKLIST = [
+    'Электрозаводская',
+    'Бауманская',
+    'Солнцево',
+]
+METRO_BLACKLIST = [m.lower() for m in METRO_BLACKLIST]
 
-def price_per_person(flat):
-    return flat.price / flat.n_rooms <= 35000
+
+def filter_price_per_person(flat):
+    ppp = flat.price / flat.rooms
+    if ppp > 35000:
+        logger.debug(
+            f'Flat {flat.id} failed price test: price={flat.price}, rooms={flat.rooms}'
+        )
+    return ppp <= 35000
+
+
+def filter_metro(flat):
+    ok = any(m.lower() in METRO
+                 for m in flat.metros) and not any(m.lower() in METRO_BLACKLIST
+                                                   for m in flat.metros)
+    if not ok:
+        logger.debug(
+            f'Flat {flat.id} failed metro test. Metros: {flat.metros}')
+    return ok
+
+
+@attr.s
+class CianStateSerializable:
+    flatlist = attr.ib(type=dict)
+    flat_details = attr.ib(type=dict)
+    viewed = attr.ib(type=dict)
+    scheduled_messages = attr.ib(type=list)
 
 
 class CianBot:
@@ -29,58 +76,79 @@ class CianBot:
         self.flat_details = dict()
         self.viewed = collections.defaultdict(set)  # chat_id -> set[int]
         self.scheduled_messages = collections.deque()
-        self.filters = []
+
+    @property
+    def filters(self):
+        return [filter_price_per_person, filter_metro]
 
     def save(self, basepath):
         if not osp.exists(basepath):
             os.makedirs(basepath)
-        with open(osp.join(basepath, 'flatlist.json'), 'w') as f:
-            json.dump(self.flatlist, f)
-        with open(osp.join(basepath, 'flat_details.json'), 'w') as f:
-            json.dump(self.flat_details, f)
-        with open(osp.join(basepath, 'scheduled_messages.json'), 'w') as f:
-            json.dump(list(self.scheduled_messages), f)
-        with open(osp.join(basepath, 'user_viewed_flats.json'), 'w') as f:
-            json.dump(list(self.viewed), f)
+        with open(osp.join(basepath, 'state.json'), 'w') as f:
+            json.dump(
+                attr.asdict(
+                    CianStateSerializable(flatlist=self.flatlist,
+                                          flat_details=self.flat_details,
+                                          viewed=self.viewed,
+                                          scheduled_messages=list(
+                                              self.scheduled_messages))), f)
 
     @staticmethod
     def from_directory(basepath):
         self = CianBot()
-        with open(osp.join(basepath, 'flatlist.json'), 'r') as f:
-            self.flatlist.update(json.load(f))
-        with open(osp.join(basepath, 'flat_details.json'), 'r') as f:
-            self.flat_details.update(json.load(f))
-        with open(osp.join(basepath, 'user_viewed_flats.json'), 'r') as f:
-            self.viewed.update(json.load(f))
-        with open(osp.join(basepath, 'scheduled_messages.json'), 'r') as f:
-            self.scheduled_messages.extend(json.load(f))
+        with open(osp.join(basepath, 'state.json'), 'r') as f:
+            state = json.load(f)
+        self.flatlist.update(state['flatlist'])
+        self.flat_details.update(state['flat_details'])
+        self.viewed.update({a: set(b) for a, b in state['viewed'].items()})
+        self.scheduled_messages.extend(state['scheduled_messages'])
         return self
 
     def start(self, update, context):
-        self.viewed[update.message.chat_id]
-        if 'job' not in context.chat_data:
-            context.chat_data['job'] = context.job_queue.run_repeating
+        self.viewed[update.message.chat_id] = set()
+        logger.info(f'{update.message.chat_id} connected')
 
-    def handle_new_flat(self, flat: cian_parser.FlatListItem):
-        if flat.id not in self.flatlist:
-            self.flatlist[flat.id] = flat
-        if not all(f(flat) for f in self.filters):
-            return
+    def flat_to_message(self, flat):
         text = '.\n'.join([
             f'{flat.pdf_link}',
-            f'{flat.price}, deposit {flat.deposit}, fee {flat.fee}',
+            ', '.join([
+                f'{k} {getattr(flat, k.lower())}'
+                for k in ['Price', 'deposit', 'fee', 'bonus']
+                if getattr(flat, k.lower())
+            ]),
+            f'{flat.bedrooms} rooms',
             f'{flat.metros}',
         ])
+        msg = dict(text=text)
+        if len(flat.photos) > 0:
+            msg['photo'] = flat.photos[0]
+        return msg
+
+    def flat_ok(self, flat):
+        for f in self.filters:
+            if not f(flat):
+                logger.debug(f'Flat {flat.id} couldn\'t pass {f.__name__}.')
+                return False
+        return True
+
+    def handle_new_flat(self, flat: cian_parser.FlatListItem):
+        if flat.id in self.flatlist:
+            return
+        self.flatlist[flat.id] = flat
+        if not self.flat_ok(flat):
+            return
+        msg = self.flat_to_message(flat)
         for u in self.viewed:
             if flat.id in self.viewed[u]:
                 continue
-            msg = dict(chat_id=u, text=text)
-            if len(flat.photos) > 0:
-                msg['photo'] = flat.photos[0]
+            msg = copy.deepcopy(msg)
+            msg['chat_id'] = u
             self.scheduled_messages.append(msg)
+            self.viewed[u].add(flat.id)
 
     def send_messages(self, context):
         while len(self.scheduled_messages) > 0:
+            logger.debug(f'Notifying {msg["chat_id"]} about: {msg["text"]}')
             msg = self.scheduled_messages.popleft()
             if 'photo' in msg:
                 context.bot.send_photo(msg['chat_id'],
@@ -89,20 +157,51 @@ class CianBot:
             else:
                 context.bot.send_message(msg['chat_id'], msg['text'])
 
+    def fetch_messages(self, update, context):
+        logger.info(f'{update.message.chat_id} asks for messages')
+        for f in self.flatlist:
+            if f.id in self.viewed[update.message.chat_id]:
+                continue
+            if not self.flat_ok(flat):
+                continue
+            msg = self.flat_to_message(f)
+            msg['chat_id'] = update.message.chat_id
+            self.scheduled_messages.append(msg)
+        logger.info('Sending messages as requested')
+        self.send_messages()
+        logger.info('Messages sent')
+
     def fetch_cian(self, context):
-        for p in range(1, 4):
+        logger.info('Fetching cian')
+        for p in range(1, 20):
             with requests.Session() as s:
-                html = cian_parser.get_flatlist_html(s, p, 300_000)
+                html = cian_parser.get_flatlist_html(s, p, 100_000)
             flats = cian_parser.get_flatlist(html)
             for f in flats:
                 self.handle_new_flat(f)
+            logger.info(f'Fetched page {p}')
+            logger.info('Sending messages after fetch')
+            self.send_messages(context)
+            logger.info('Messages sent')
+        logger.info('Saving backup')
+        self.save('.cian-backup')
+        logger.info('Saved backup')
+    def fetch_cian_url(self, context):
+        with requests.Session() as s:
+            html = s.get(url)
+        flats = cian_parser.get_flatlist(html)
+        for f in flats:
+            self.handle_new_flat(f)
+        logger.info(f'Fetched page {p}')
+        logger.info('Sending messages after fetch')
         self.send_messages(context)
+        logger.info('Messages sent')
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('cian_bot')
     parser.add_argument('--token-file', default='.token')
-    parser.add_argument('--user', default=[], action='append')
     parser.add_argument('--state-dir', default='cian')
 
     args = parser.parse_args()
@@ -119,8 +218,9 @@ if __name__ == '__main__':
 
     try:
         job = updater.job_queue
-        job.run_repeating(state.fetch_cian, datetime.timedelta(minutes=30), 10)
+        job.run_repeating(state.fetch_cian, datetime.timedelta(minutes=180), 10)
         dp.add_handler(CommandHandler('start', state.start))
+        dp.add_handler(CommandHandler('fetch', state.fetch_messages))
         updater.start_polling()
         updater.idle()
     finally:
--- src/cian_parser/__init__.py
+++ src/cian_parser/__init__.py
@@ -4,6 +4,7 @@ import itertools
 import json
 import pprint
 import re
+import logging
 from urllib.parse import urljoin, urlparse
 
 import attr
@@ -12,23 +13,96 @@ import requests
 import pyjsparser
 from bs4 import BeautifulSoup
 
+logger = logging.getLogger('cian_bot.cian_parser')
+
 OFFER_ID_PATTERN = re.compile(r'\bID (?P<id>[a-zA-Z0-9]+)\b')
 EXAMPLE_URL = 'https://www.cian.ru/cat.php?deal_type=rent&maxprice={maxprice}&engine_version=2&foot_min=45&metro%5B0%5D=54&metro%5B10%5D=132&metro%5B11%5D=145&metro%5B12%5D=148&metro%5B13%5D=149&metro%5B14%5D=237&metro%5B1%5D=58&metro%5B2%5D=68&metro%5B3%5D=71&metro%5B4%5D=78&metro%5B5%5D=103&metro%5B6%5D=105&metro%5B7%5D=119&metro%5B8%5D=121&metro%5B9%5D=130&offer_type=flat&only_foot=2&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1&type=4&p={page}'
 BASE_URL = 'https://www.cian.ru/cat.php'
 
-URL_DEFAULTS = dict(deal_type='rent',
-                    maxprice=1_000_000,
-                    engine_version=2,
-                    foot_min=30,
-                    offer_type='flat',
-                    room1=1,
-                    room2=1,
-                    room3=1,
-                    room4=1,
-                    room5=1,
-                    room6=1,
-                    type=4,
-                    p=1)
+URL_DEFAULTS = dict(
+    deal_type='rent',
+    maxprice=1_000_000,
+    engine_version=2,
+    foot_min=20,
+    offer_type='flat',
+    room2=1,
+    room3=1,
+    minprice=15_000,
+    # room4=1,
+    # room5=1,
+    # room6=1,
+    sort='price_object_order',
+    # metro=[
+    #     '47', '121', '130', '132', '145', '148', '149', '150', '237', '54',
+    #     '58', '64', '68', '71', '78', '103', '105', '119'],
+    p=1)
+URL_DEFAULTS.update({
+    'metro[0]': '47',
+    'metro[10]': '121',
+    'metro[11]': '130',
+    'metro[12]': '132',
+    'metro[13]': '145',
+    'metro[14]': '148',
+    'metro[15]': '149',
+    'metro[16]': '150',
+    'metro[17]': '237',
+    'metro[1]': '54',
+    'metro[2]': '58',
+    'metro[3]': '64',
+    'metro[4]': '68',
+    'metro[5]': '71',
+    'metro[6]': '78',
+    'metro[7]': '103',
+    'metro[8]': '105',
+    'metro[9]': '119'
+})
+
+# Within the ring only
+# URL_DEFAULTS.update({
+#     'metro[0]': '4',
+#     'metro[10]': '54',
+#     'metro[11]': '56',
+#     'metro[12]': '58',
+#     'metro[13]': '61',
+#     'metro[14]': '64',
+#     'metro[15]': '66',
+#     'metro[16]': '68',
+#     'metro[17]': '71',
+#     'metro[18]': '77',
+#     'metro[19]': '78',
+#     'metro[1]': '8',
+#     'metro[20]': '80',
+#     'metro[21]': '84',
+#     'metro[22]': '85',
+#     'metro[23]': '86',
+#     'metro[24]': '96',
+#     'metro[25]': '98',
+#     'metro[26]': '103',
+#     'metro[27]': '105',
+#     'metro[28]': '114',
+#     'metro[29]': '115',
+#     'metro[2]': '12',
+#     'metro[30]': '119',
+#     'metro[31]': '121',
+#     'metro[32]': '123',
+#     'metro[33]': '124',
+#     'metro[34]': '125',
+#     'metro[35]': '129',
+#     'metro[36]': '130',
+#     'metro[37]': '132',
+#     'metro[38]': '145',
+#     'metro[39]': '148',
+#     'metro[3]': '15',
+#     'metro[40]': '149',
+#     'metro[41]': '150',
+#     'metro[42]': '159',
+#     'metro[4]': '18',
+#     'metro[5]': '20',
+#     'metro[6]': '38',
+#     'metro[7]': '46',
+#     'metro[8]': '47',
+#     'metro[9]': '50'
+# })
 
 
 def offer_container_to_id_href(offer_soup):
@@ -57,9 +131,9 @@ class FlatListItem:
     id = attr.ib(type=int)
     href = attr.ib()
     pdf_link = attr.ib()
-    price = attr.ib()
-    deposit = attr.ib()
-    fee = attr.ib()
+    price = attr.ib(type=float)
+    deposit = attr.ib(type=float)
+    fee = attr.ib(type=float)
     bonus = attr.ib()
     metros = attr.ib(type=list)
     rooms = attr.ib(type=int)
@@ -72,18 +146,19 @@ class FlatListItem:
 
 def get_params(**params):
     pp = copy.deepcopy(URL_DEFAULTS)
-    pp.update(params)
+    pp = pp.update(params)
     return pp
 
 
 def get_flatlist_html(req, page, maxprice):
     res = req.get(BASE_URL, params=get_params(p=page, maxprice=maxprice))
+    logger.debug(f'Finished querying {res.url}. Status {res.status_code}')
     res = res.text
     return res
 
 
 def get_flatlist(html):
-    res = BeautifulSoup(html)
+    res = BeautifulSoup(html, 'lxml')
     js = pyjsparser.parse(
         next(s for s in res.find_all('script') if '"priceRur"' in s.text).text)
     offers = next(o['value'] for t, o in js_traverse(js)
@@ -94,15 +169,20 @@ def get_flatlist(html):
             int(o['id']), o['fullUrl'],
             urljoin('https://cian.ru/export/pdf/',
                     urlparse(o['fullUrl']).path[1:]),
-            o['bargainTerms']['priceRur'], o['bargainTerms']['deposit'],
-            o['bargainTerms']['clientFee'],
-            (o['bargainTerms']['agentBonus'] or 0),
+            (o['bargainTerms']['priceRur'] if o['bargainTerms']['priceRur'] >
+             5000 else o['bargainTerms']['priceRur'] * 65),
+            o['bargainTerms']['deposit'], o['bargainTerms']['clientFee'],
+            (o['bargainTerms'].get('agentBonus', 0) or 0),
             [ug['name'] for ug in o['geo']['undergrounds']],
             int(o['roomsCount'] or 1),
-            int(o['bedroomsCount'] or max(1,
-                                           int(o['roomsCount']) - 1)),
-            o['description'], o['geo']['userInput'],
-            [p['fullUrl'] for p in o['photos']], o) for o in offers
+            int(o['bedroomsCount']
+                or max(1,
+                       int(o['roomsCount'] or 0) - 1)),
+            o['description'],
+            o['geo']['userInput'],
+            [p['fullUrl'] for p in o['photos']],
+            o)
+        for o in offers
     ]
 
 
@@ -169,7 +249,7 @@ def js_findall_offer_data(js):
 
 
 def get_flats(html):
-    page = BeautifulSoup(html)
+    page = BeautifulSoup(html, 'lxml')
     js = next(s for s in page.find_all('script') if '"offerId"' in s.text).text
     js = pyjsparser.parse(js)
     yield from js_findall_offer_data(js)
